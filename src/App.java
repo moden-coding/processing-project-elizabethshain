6023a62bde9a6c0f0ad25416c5b5b44940ea34ab
elizabethshain <29eshain@gannacademy.org>
2026-03-06 10:48:58 -0500

    work

--- src/App.java
+++ src/App.java
@@ -1,20 +1,117 @@
 import processing.core.*;
 
-public class App extends PApplet{
-    public static void main(String[] args)  {
+public class App extends PApplet {
+    int highscore = 0;
+    int scene = 1;
+    int count = 0;
+    int diameter = 100 / 2;
+    int circ1x = 100;
+    int circ1y = 0;
+    int circ2x = 200;
+    int circ2y = 0;
+    int circ3x = 300;
+    int circ3y = 0;
+    int circ4x = 400;
+    int circ4y = 0;
+    int circ5x = 500;
+    int circ5y = 0;
+
+    public static void main(String[] args) {
         PApplet.main("App");
     }
 
-    public void setup(){
-        
+    public void setup() {
+
     }
 
-    public void settings(){
-        
+    public void settings() {
+        size(600, 500);
     }
 
-    public void draw(){
-       
+    public void draw() {
+        if (scene == 1) {
+            background(30, 30, 30);
+            circle(circ1x, circ1y, diameter);
+            fill(200, 0, 0);
+            circle(circ2x, circ2y, diameter);
+            fill(0, 200, 0);
+            circle(circ3x, circ3y, diameter);
+            fill(0, 0, 200);
+            circle(circ4x, circ4y, diameter);
+            fill(200, 0, 200);
+            circle(circ5x, circ5y, diameter);
+            fill(0, 200, 200);
 
+            if (count > highscore) {
+                highscore = count;
+            }
+            circ1y = circ1y + 2;
+            if (circ1y > 500)
+                scene += 1;
+
+            circ2y = circ2y + 2;
+            if (circ2y > 500)
+                scene += 1;
+
+            circ3y = circ3y + 2;
+            if (circ3y > 500)
+                scene += 1;
+
+            circ4y = circ4y + 2;
+            if (circ4y > 500)
+                scene += 1;
+
+            circ5y = circ5y + 2;
+            if (circ5y > 500)
+                scene += 1;
+        }
+        if (scene == 2) {
+            background(0);
+            textSize(50);
+            text("HAHA! YOU LOSE!", 3 + 100, 250);
+            textSize(40);
+            text("Circles tapped: " + count, 3 + 100, 100);
+            textSize(40);
+            text("Highscore: " + highscore, 3 + 100, 50);
+        }
+        if (scene == 3) {
+
+        }
+    }
+
+    public void mousePressed() {
+        float distance = dist(circ1x, circ1y, mouseX, mouseY);
+        if (distance < diameter) {
+            circ1x = (int) random(500);
+            circ1y = -100;
+            count++;
+        }
+        float distance2 = dist(circ2x, circ2y, mouseX, mouseY);
+        if (distance2 < diameter) {
+            circ2x = (int) random(500);
+            circ2y = -100;
+            count++;
+        }
+        float distance3 = dist(circ3x, circ3y, mouseX, mouseY);
+        if (distance3 < diameter) {
+            circ3x = (int) random(500);
+            circ3y = -100;
+            count++;
+        }
+        float distance4 = dist(circ4x, circ4y, mouseX, mouseY);
+        if (distance4 < diameter) {
+            circ4x = (int) random(500);
+            circ4y = -100;
+            count++;
+        }
+        float distance5 = dist(circ5x, circ5y, mouseX, mouseY);
+        if (distance5 < diameter) {
+            circ5x = (int) random(500);
+            circ5y = -100;
+            count++;
+        }
+        if (mousePressed && scene == 2) {
+            scene = 1;
+        }
     }
 }
